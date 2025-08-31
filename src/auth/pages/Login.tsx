108f5a9aee89932fd427d66bb16afe1455b359da
neumanf <fabricionewman@protonmail.com>
2025-08-31 20:48:32 -0300

    chore: debug auth0 config

--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,15 +1,19 @@
 import { Navigate } from "react-router-dom";
 import { Button } from "primereact/button";
 import { useAuth } from "../../shared/hooks/useAuth";
+import { auth0Config } from "../config/auth0-config";
 
 const Login = () => {
   const { isLoading, isAuthenticated, login, register } = useAuth();
 
+  const config = JSON.stringify(auth0Config);
+
   return (
     <div>
-      { isLoading && <div>Loading...</div>}
+      {isLoading && <div>Loading...</div>}
       {isAuthenticated === false && (
         <>
+          Config: {config}
           <div>
             <Button
               onClick={() => login()}
